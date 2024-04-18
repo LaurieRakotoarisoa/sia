ba64061f23583f7a34c9d008ba23f1804782a6eb
Laurie <laurierakoto@aol.com>
2024-04-18 20:31:49 +0200

    Handling data with Spring JPA

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -1,12 +1,16 @@
 package tacos;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@Table
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE,force = true)
 public class Ingredient {
 	
 	@Id
